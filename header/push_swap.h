dac8944b2665c0495d4a83f9ba083637ded7488a
doomangang <sp0943@cau.ac.kr>
2024-07-24 19:19:55 +0900

    mod : header order

--- header/push_swap.h
+++ header/push_swap.h
@@ -40,12 +40,12 @@ int		count_height(char **arr);
 int		free_arr(char **arr);
 void	print_error(void);
 int		my_atoi(char *str, int *flag);
-int		sorted(int *num, int size);
 int		init_ab(t_ab *ab, int *arr, int size);
 void	under_five(t_ab *ab, int size);
 void	sort_three(t_stack *s);
-int		convert(t_ab *ab);
 int		*sort(t_stack *s);
+int		sorted(int *num, int size);
+int		convert(t_ab *ab);
 void	copy(t_stack *s, int *t);
 void	push_swap(t_ab *ab, int *num, int size);
 void	a_to_b(t_ab *ab, int ch);
@@ -60,12 +60,12 @@ int		push(t_ab *ab, char c);
 int		is_empty(t_stack *s);
 int		is_full(t_stack *s);
 void	delete_ab(t_ab *ab);
-int		get_max(t_stack *s);
 void	add_front(t_stack *s, int val);
 void	add_rear(t_stack *s, int val);
 int		pop_front(t_stack *s);
 int		pop_rear(t_stack *s);
 int		get_front(t_stack *s);
 int		get_rear(t_stack *s);
+int		get_max(t_stack *s);
 
 #endif
